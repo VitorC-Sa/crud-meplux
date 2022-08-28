2c4a3902fc29cac6069171d6495626d374ca0955
VCarlosSoares <vitorcarlos2014soares@gmail.com>
2022-08-28 15:34:17 -0300

    feat: client controller get function

--- controllers/clientController/clientController.go
+++ controllers/clientController/clientController.go
@@ -2,14 +2,32 @@ package clientController
 
 import (
 	"net/http"
+	"strconv"
 
+	c "github.com/VitorC-sa/crud-meplux/models/clientModel"
 	"github.com/gin-gonic/gin"
 )
 
 func GET(ctx *gin.Context) {
-	ctx.JSON(http.StatusAccepted, gin.H{
-		"status": "OK",
-	})
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"codigo":   "Requisicao Invalida",
+			"mensagem": "ID fora do formato esperado",
+		})
+		return
+	}
+
+	c := c.GetOne(id)
+	if c == nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"codigo":   "Entidade invalida",
+			"mensagem": "Cliente nao encontrado",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, c)
 }
 
 func GETALL(ctx *gin.Context) {
